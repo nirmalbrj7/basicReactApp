d9754840cd275c8b84b6284b3e276f6444333675
Nirmal Adhikari <nirmalbrj7@gmail.com>
2020-05-03 21:34:21 +0545

    Updates

---
+++ precache-manifest.88e41bf4fe3324b2ae000323ff872479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ee7d955000d14b3ec7a2b0cdf9e6bd",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/index.html"
+  },
+  {
+    "revision": "ad86ae5435be1903cfa2",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/static/css/main.fbe244f3.chunk.css"
+  },
+  {
+    "revision": "1b163c6dfb305b2c587f",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/static/js/2.b0e7918a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/static/js/2.b0e7918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad86ae5435be1903cfa2",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/static/js/main.eecea306.chunk.js"
+  },
+  {
+    "revision": "c8a143dfa871ed978732",
+    "url": "/https//nirmalbrj7.github.io/basicReactApp/static/js/runtime-main.6cfa711d.js"
+  }
+]);
